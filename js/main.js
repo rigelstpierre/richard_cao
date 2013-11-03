06a7fb43ac86c3c547a31ae4e5aede4517c9c9a1
Rigel St. Pierre <rigel@amcoonline.net>
2013-11-03 11:03:14 -0800

    sorting, adds blog page

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$( document ).ready(function() {
 
+  $('#weddings').click(function() {
+    $('.wedding').fadeIn();
+    $('.portraights, .family').fadeOut();
+  });
+
+  $('#portraights').click(function() {
+    $('.portraights').fadeIn();
+    $('.wedding, .family').fadeOut();
+  });
+
+  $('#family').click(function() {
+    $('.family').fadeIn();
+    $('.portraights, .wedding').fadeOut();
+  });
+
+});
